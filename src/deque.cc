a57de345bcbf063b972d798113069843cfe067ec
Eros Rolfe <erosrolf@un-o3.kzn.21-school.ru>
2023-12-26 13:17:09 +0300

    added header

--- src/deque.cc
+++
@@ -1,3 +0,0 @@
-#include "deque.h"
-
-int main() { s21::deque myDeque; }
---
+++ src/s21_stack.h
@@ -0,0 +1,35 @@
+#ifndef S21_CONTAINERS_1_SRC_S21_STACK_H_
+#define S21_CONTAINERS_1_SRC_S21_STACK_H_
+
+#include <deque>
+
+namespace s21 {
+
+template <class T, std::deque<T>> class stack {
+public:
+  using value_type = T;
+  using reference = T &;
+  using const_reference = const T &;
+  using size_type = std::size_t;
+
+  stack();
+  stack(std::initializer_list<value_type> const &items);
+  stack(const stack &s);
+  stack(stack &&s);
+  ~stack();
+  // operator=(stack &&s);
+
+  const_reference top();
+
+  bool empty();
+  size_type size();
+
+  void push(const_reference value);
+  void pop();
+  void swap(stack &other);
+
+}; // stack
+
+} // namespace s21
+
+#endif // S21_CONTAINERS_1_SRC_S21_STACK_H_
