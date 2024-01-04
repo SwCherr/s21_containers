bdc7fae5d2257cee61aa15caeb91ea0594a9bc76
Eros Rolfe <erosrolf@un-o3.kzn.21-school.ru>
2024-01-04 19:28:13 +0300

    make abstract class container_adaptor

--- src/s21_stack.h
+++ src/s21_stack.h
@@ -2,14 +2,37 @@
 #define S21_CONTAINERS_1_SRC_S21_STACK_H_
 
 #include <deque>
-#include <iostream>
-#include <list>
-#include <stack>
 
 namespace s21 {
 
 template <class T, class C = std::deque<T>>
-class stack {
+class container_adaptor {
+ public:
+  using value_type = T;
+  using reference = T &;
+  using const_reference = const T &;
+  using size_type = std::size_t;
+
+  const_reference top();
+  bool empty();
+
+ protected:
+  C container_;
+};
+
+template <class T, class C>
+typename container_adaptor<T, C>::const_reference
+container_adaptor<T, C>::top() {
+  return this->container_.back();
+}
+
+template <class T, class C>
+bool container_adaptor<T, C>::empty() {
+  return container_.empty();
+}
+
+template <class T, class C = std::deque<T>>
+class stack : public container_adaptor<T, C> {
  public:
   using value_type = T;
   using reference = T &;
@@ -22,19 +45,13 @@ class stack {
   stack(stack &&other) noexcept;
   ~stack() = default;
 
+  size_type size();
+  void push(const_reference value);
+  void swap(stack &other) noexcept;
+  void pop();
+
   stack operator=(const stack &other);
   bool operator==(const stack &other);
-
-  const_reference top();
-  bool empty();
-  size_type size();
-
-  void push(const_reference value);
-  void pop();
-  void swap(stack &other) noexcept;
-
- private:
-  C container_;
 };  // class stack
 
 template <class T, class C>
@@ -45,51 +62,44 @@ stack<T, C>::stack(std::initializer_list<value_type> const &items) {
 }
 
 template <class T, class C>
-stack<T, C>::stack(const stack &other) : container_(other.container_) {}
+stack<T, C>::stack(const stack &other) {
+  this->container_ = other.container_;
+}
 
 template <class T, class C>
-stack<T, C>::stack(stack &&other) noexcept
-    : container_(std::move(other.container_)) {}
+stack<T, C>::stack(stack &&other) noexcept {
+  this->container_ = std::move(other.container_);
+}
+
+template <class T, class C>
+typename stack<T, C>::size_type stack<T, C>::size() {
+  return this->container_.size();
+}
+
+template <class T, class C>
+void stack<T, C>::push(const_reference value) {
+  this->container_.push_back(value);
+}
+
+template <class T, class C>
+void stack<T, C>::pop() {
+  this->container_.pop_back();
+}
+
+template <class T, class C>
+void stack<T, C>::swap(stack &other) noexcept {
+  this->container_.swap(other.container_);
+}
 
 template <class T, class C>
 stack<T, C> stack<T, C>::operator=(const stack &other) {
-  container_ = other.container_;
+  this->container_ = other.container_;
   return *this;
 }
 
 template <class T, class C>
 bool stack<T, C>::operator==(const stack &other) {
-  return container_ == other.container_;
-}
-
-template <class T, class C>
-typename stack<T, C>::const_reference stack<T, C>::top() {
-  return container_.back();
-}
-
-template <class T, class C>
-bool stack<T, C>::empty() {
-  return container_.empty();
-}
-
-template <class T, class C>
-typename stack<T, C>::size_type stack<T, C>::size() {
-  return container_.size();
-}
-
-template <class T, class C>
-void stack<T, C>::push(const_reference value) {
-  container_.push_back(value);
-}
-
-template <class T, class C>
-void stack<T, C>::pop() {
-  container_.pop_back();
-}
-
-template <class T, class C>
-void stack<T, C>::swap(stack &other) noexcept {
-  container_.swap(other.container_);
+  return this->container_ == other.container_;
 }
 
 }  // namespace s21
