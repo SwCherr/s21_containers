ea92bde6c42ab99162b8e0c9495d6fc6e77516b4
Eros Rolfe <erosrolf@un-o3.kzn.21-school.ru>
2024-01-19 15:46:29 +0300

    fixed include naming

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,7 +1,7 @@
 #ifndef CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
 #define CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
 
-#include "s21_deque.h"
+#include "s21_queue.h"
 #include "s21_stack.h"
 
 #endif  // CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
