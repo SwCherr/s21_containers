4513817b949f7cad6b51de8f7cad1e2baa381b77
Eros Rolfe <erosrolf@un-o3.kzn.21-school.ru>
2024-02-01 14:12:50 +0300

    added swap, fill

--- src/s21_array_iterator.h
+++ src/s21_array_iterator.h
@@ -12,6 +12,8 @@ class ArrayIterator {
   using reference = T &;
   using pointer = T *;
   using size_type = std::size_t;
+  using iterator = ArrayIterator<T>;
+  using const_iterator = const ArrayIterator<T>;
 
   ArrayIterator(pointer arr_ptr) : arr_ptr_(arr_ptr), position_(0) {}
   ArrayIterator(pointer arr_ptr, size_type size)
@@ -23,33 +25,55 @@ class ArrayIterator {
   bool operator==(const ArrayIterator &other) const {
     return this->arr_ptr_ == other.arr_ptr_;
   }
-  ArrayIterator &operator++() {
+
+  pointer operator->() const { return arr_ptr_; }
+
+  iterator operator++() {
     ++position_;
     return *this;
   }
-  ArrayIterator &operator++(int) {
-    ++position_;
-    return *this;
+
+  iterator operator++(int) {
+    iterator tmp(arr_ptr_, ++position_);
+    return tmp;
   }
-  ArrayIterator &operator+(int index) {
+
+  const_iterator operator++(int) const {
+    size_type new_pos = position_ + 1;
+    iterator tmp(arr_ptr_, new_pos);
+    return tmp;
+  }
+
+  iterator operator+(int index) {
     position_ += index;
     return *this;
   }
-  ArrayIterator<T> &operator-(int index) {
+
+  iterator operator-(int index) {
     position_ -= index;
     return *this;
   }
+
   size_type operator-(const ArrayIterator &other) const {
     return position_ - other.position_;
   }
-  ArrayIterator &operator--() {
+
+  iterator operator--() {
     --position_;
     return *this;
   }
-  ArrayIterator &operator--(int) {
-    --position_;
-    return *this;
+
+  iterator operator--(int) {
+    iterator tmp(arr_ptr_, --position_);
+    return tmp;
   }
+
+  const_iterator operator--(int) const {
+    size_type new_pos = position_ - 1;
+    iterator tmp(arr_ptr_, new_pos);
+    return tmp;
+  }
+
   reference operator*() const { return *(arr_ptr_ + position_); }
 
  private:
